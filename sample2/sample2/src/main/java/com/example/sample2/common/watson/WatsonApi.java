08235f7244c68c27492a49aea46d2ae88d6d4b27
jihoon8508 <jihoon8508@nate.com>
2022-03-16 14:51:38 +0900

    IBM WATSON 커밋

---
+++ sample2/sample2/src/main/java/com/example/sample2/common/watson/WatsonApi.java
@@ -0,0 +1,72 @@
+package com.example.sample2.common.watson;
+
+import com.ibm.cloud.sdk.core.security.Authenticator;
+import com.ibm.cloud.sdk.core.security.BasicAuthenticator;
+import com.ibm.watson.discovery.v1.Discovery;
+
+public class WatsonApi {
+    public void watsonConnector() {
+        Authenticator authenticator = new BasicAuthenticator.Builder()
+                                                            .username("username")
+                                                            .password("password")
+                                                            .build();
+
+        Discovery service = new Discovery("2022-03-16", authenticator);
+
+        // Response<WorkspaceCollection> response = service.get
+
+        // WorkspaceCollection collection = response.getResult();
+
+        // System.out.println("My workspace : " + collection.getWorkspaces());
+    }
+}
+
+
+/*
+public class ConnectWatsonAssistant {
+
+	public static void main(String[] args) {
+
+		String apiKey = "rH*****************************8nV";
+		String version = "2021-08-18";
+		String serviceUrl = "https://api.us-south.assistant.watson.cloud.ibm.com/instances/******************";
+		String assistantId = "25**********************5";
+		
+		IamAuthenticator authenticator = new IamAuthenticator(apiKey);
+		Assistant assistant = new Assistant(version, authenticator);
+		assistant.setServiceUrl(serviceUrl);
+		CreateSessionOptions options = new CreateSessionOptions.Builder(assistantId).build();
+		assistant.createSession(options);
+		SessionResponse response = assistant.createSession(options).execute().getResult();
+
+		System.out.println("Session="+response);
+
+		MessageInput input = new MessageInput.Builder().text("I am not feeling well.").build();
+		MessageOptions messageOptions = new MessageOptions.Builder().assistantId(assistantId)
+				.sessionId(response.getSessionId()).input(input).build();
+		MessageResponse messageResponse = assistant.message(messageOptions).execute().getResult();
+
+		System.out.println("ResponseFromWatson=" + messageResponse);
+
+	}
+
+}
+*/
+
+/*
+public static void main(String[] args) {
+    IamOptions iamOptions = new IamOptions.Builder().apiKey("{apikey}").build();
+    Assistant service = new Assistant("2018-09-20", iamOptions);
+    service.setEndPoint("https://gateway.watsonplatform.net/assistant/api");
+
+    String workspaceId = "{workspaceId}";
+    InputData input = new InputData.Builder("다이하드 보러갈래?").build();
+
+    MessageOptions options = new MessageOptions.Builder(workspaceId)
+      .input(input)
+      .build();
+
+    MessageResponse response = service.message(options).execute();
+    System.out.println(response);
+}
+*/
